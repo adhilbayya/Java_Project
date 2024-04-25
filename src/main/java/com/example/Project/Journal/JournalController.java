9e711da09471d89127900306ebba3e99672d2d42
AdhilBayya <adhilvk445@gmail.com>
2024-04-25 18:08:20 +0530

    Added CRUD operations

--- src/main/java/com/example/Project/Journal/JournalController.java
+++ src/main/java/com/example/Project/Journal/JournalController.java
@@ -4,24 +4,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "api/journal")
 public class JournalController {
     private final JournalService journalService;
+    JournalRepository journalRepository;
 
     @Autowired
     public JournalController(JournalService journalService) {
         this.journalService = journalService;
     }
 
-    @GetMapping
+    @GetMapping("/get-all-journal")
     public List<Journal> getJournals() {
         return journalService.getJournal();
     }
 
-    @PostMapping
-    public void addJournal(@RequestBody Journal journal) {
-        journalService.addNewJournal(journal);
+    @GetMapping("/journal/{id}")
+    public Journal getJournal(@PathVariable("id") long id) {
+        return journalService.getJournal(id);
+    }
+
+    @DeleteMapping("/remove/{journalID}")
+    public boolean deleteJournal(@PathVariable("journalID") long id){
+        return journalService.deleteJournal(id);
+    }
+
+    @PutMapping("update/{id}")
+    public Journal updateJournal(@PathVariable("id") long id, @RequestBody Map<String, String> journal){
+        return journalService.updateJournal(id, journal);
+    }
+
+    @PostMapping("/add")
+    public Journal addJournal(@RequestBody Map<String, String> journal){
+        return journalService.addNewJournal(journal);
     }
 }
--- src/main/java/com/example/Project/Journal/JournalService.java
+++ src/main/java/com/example/Project/Journal/JournalService.java
@@ -2,8 +2,12 @@ package com.example.Project.Journal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class JournalService {
@@ -18,7 +22,31 @@ public class JournalService {
         return journalRepository.findAll();
     }
 
-    public void addNewJournal(Journal journal) {
-        System.out.println(journal);
+    public Journal addNewJournal(Map<String, String> journal) {
+        String title = journal.get("title");
+        String description = journal.get("description");
+        Journal newJournal = new Journal(title, description);
+        journalRepository.save(newJournal);
+        return newJournal;
+    }
+
+    public Journal getJournal(@PathVariable("id") long id) {
+        return journalRepository.findById(id).get();
+    }
+
+    public boolean deleteJournal(@PathVariable("journalID") long id){
+        if(!journalRepository.findById(id).equals(Optional.empty())){
+            journalRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public Journal updateJournal(Long id, @RequestBody Map<String, String> journal) {
+        Journal jo = journalRepository.findById(id).get();
+        jo.setJournalTitle(journal.get("journalTitle"));
+        jo.setJournalDescription(journal.get("journalDescription"));
+        journalRepository.save(jo);
+        return jo;
     }
 }
